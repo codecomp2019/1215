b2681c723201d34b31d3fa24bc6b2944fddd4af3
Dallin Robbins <zeidokku@gmail.com>
2019-02-02 08:32:07 -0800

    Create Master

---
+++ app/src/main/java/com/team8/memesplaining/MainActivity.java
@@ -0,0 +1,15 @@
+package com.team8.memesplaining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
