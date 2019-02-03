04d14db06d356e2a633d04a51d3a06d620347a9f
Dallin Robbins <zeidokku@gmail.com>
2019-02-02 20:33:59 -0800

    More code, more bugs

--- app/src/main/java/com/team8/memesplaining/MainActivity.java
+++ app/src/main/java/com/team8/memesplaining/MainActivity.java
@@ -1,25 +1,33 @@
 package com.team8.memesplaining;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity
 
 	private static final int READ_REQUEST_CODE = 42;
 	private static final String TAG = "MainActivity";
+	private static final int REQUEST_PERMISSION = 1;
 
 
 	@Override
@@ -71,7 +80,7 @@ public class MainActivity extends AppCompatActivity
 					FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
 					Log.i(TAG, "FirebaseVisionImage " + image);
 
-					//recognizeText(image);
+					recognizeText(image);
 
 				} catch (IOException e)
 				{
@@ -106,6 +115,7 @@ public class MainActivity extends AppCompatActivity
 												// Task completed successfully
 												// [START_EXCLUDE]
 												// [START get_text]
+
 												for (FirebaseVisionText.TextBlock block : firebaseVisionText.getTextBlocks())
 												{
 													Rect boundingBox = block.getBoundingBox();
@@ -132,6 +142,42 @@ public class MainActivity extends AppCompatActivity
 															{
 															}
 														});
-		// [END run_detector]
+
 	}
+/*
+	private void requestReadFilePermission()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+						&& ContextCompat.checkSelfPermission(mView.getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+		{
+			ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION);
+		} else
+		{
+			//permission is already granted - do what you need
+		}
+	}
+	*/
+/*
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+	                                       String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+								&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					// permission was granted, yay! Do the
+					// contacts-related task you need to do.
+				} else {
+					// permission denied, boo! Disable the
+					// functionality that depends on this permission.
+				}
+				return;
+			}
+
+			// other 'case' lines to check for other
+			// permissions this app might request.
+		}
+	}
+	*/
 }
